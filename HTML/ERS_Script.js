54c59a4ea59d2b8c3ba8756edb1ad5920c38cd0a
David Carrillo <carrillo.dact13@gmail.com>
2021-03-24 12:08:54 -0700

    edited html files

--- HTML/ERS_Script.js
+++ HTML/ERS_Script.js
@@ -2,6 +2,28 @@ function myFun(){
     var po = document.getElementById("posit").value;
     var usrn = document.getElementById("ur").value;
     var passw= document.getElementById("pass").value;
+    
+    // document.getElementById("home").innerHTML= po;
 
-    console.log(`${po} ${usrn} ${passw}`);
+    console.log(po);
+
+    
 }
+
+// var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
+// var xhrbtn = document.querySelector("#xhr");
+// var fetchbtn = document.querySelector("#fetch");
+// var axiosbtn = document.querySelector("#axios");
+// var display = document.querySelector("#quote");
+
+// fetchbtn.addEventListener("click", function(){
+//     fetch(url)
+//     .then(function(res){
+//       res.json().then(function(data){
+//         display.innerText = data[0];
+//       })
+//     })
+//     .catch(function(){
+//       alert("ERROR!");
+//     })
+//   });
